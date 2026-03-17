df15691fb0b61aa0a88d184020d1b79578523314
Bhinsar <bhinsarjagdish2003@gmail.com>
2026-03-17 12:58:40 +0530

    1727

--- Leetcode/LeetCode1878.java
+++ Leetcode/LeetCode1878.java
@@ -2,8 +2,7 @@ import java.util.TreeSet;
 
 class LeetCode1878 {
     public static void main(String[] args) {
-        int[][] arr = { { 3, 4, 5, 1, 3 }, { 3, 3, 4, 2, 3 }, { 20, 30, 200, 40, 10 }, { 1, 5, 5, 4, 1 },
-                { 4, 3, 2, 2, 5 } };
+        int[][] arr = { { 3, 4, 5, }, { 3, 3, 4 }, { 20, 30, 200} };
         int []result = getBiggestThree(arr); 
         System.out.println("result: ");
         for (int res : result) {
---
+++ Leetcode/Leetcode1727.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+
+public class Leetcode1727 {
+    public static void main(String[] args) {
+        int[][] matrix = { { 0, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 } };
+        System.out.println("result: " + largestSubmatrix(matrix));
+    }
+
+    public static int largestSubmatrix(int[][] matrix) {
+       int m = matrix.length;
+        int n = matrix[0].length;
+
+        for(int i =1; i< m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j]==0) continue;
+                matrix[i][j] += matrix[i-1][j];
+            }
+        }
+        int maxArea = 0;
+        for(int i=0; i<m; i++) {
+            Arrays.sort(matrix[i]);
+            for(int j=0; j<n; j++) {
+                int width = n-j;
+                maxArea = Math.max(maxArea, width * matrix[i][j]);
+                
+            }
+        }
+        return maxArea;
+    }
+}
