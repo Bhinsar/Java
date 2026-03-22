5f382c413648c091cfa459d35731f5a4a56a7c8f
Bhinsar <bhinsarjagdish2003@gmail.com>
2026-03-22 13:13:11 +0530

    3643

--- Leetcode/LeetCode1886.java
+++ Leetcode/LeetCode1886.java
@@ -1,7 +1,3 @@
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class LeetCode1886 {
     public static void main(String[] args) {
         int mat[][] = { { 0, 1 }, { 1, 0 } };
