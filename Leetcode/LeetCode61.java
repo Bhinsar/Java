75de58bdc242e98d064e7737320b753c696177ff
Bhinsar <bhinsarjagdish2003@gmail.com>
2026-05-05 13:15:04 +0530

    61

---
+++ Leetcode/LeetCode61.java
@@ -0,0 +1,67 @@
+
+public class LeetCode61 {
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        ListNode current = rotateRight(head, 2);
+
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+
+        System.out.println("null");
+
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        k = k % length;
+        if (k == 0)
+            return head;
+
+        ListNode newTail = head;
+        for (int i = 0; i < length - k - 1; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null; 
+        tail.next = head;
+
+        return newHead;
+    }
+
+}
