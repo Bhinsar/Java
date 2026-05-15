c75ff5ad193f33df4889cc725db718d86e838e26
Bhinsar <bhinsarjagdish2003@gmail.com>
2026-05-15 11:08:57 +0530

    153

---
+++ Leetcode/LeetCode153.java
@@ -0,0 +1,23 @@
+public class LeetCode153 {
+    public static void main(String[] args) {
+        int nums[] = { 3, 4, 5, 1, 2 };
+        System.out.println("res: " + findMin(nums));
+    }
+
+    public static int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length-1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return nums[r];
+    }
+}
