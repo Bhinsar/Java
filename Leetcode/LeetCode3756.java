f876f885649ccc49a465dc0fe10cbf72b2cfca59
Bhinsar <bhinsarjagdish2003@gmail.com>
2026-07-08 18:39:57 +0530

    3756

--- Leetcode/LeetCode3756.java
+++ Leetcode/LeetCode3756.java
@@ -1,32 +1,41 @@
 public class LeetCode3756 {
     public static void main(String[] args) {
-        int res1[] = sumAndMultiply("10203004", new int[][] { { 0, 7 }, { 1, 3 }, { 4, 6 } });
-        System.out.println("res1: ");
-        for (int n : res1) {
-            System.out.println(n);
-        }
-        int res2[] = sumAndMultiply("1000", new int[][] { { 0, 3 }, { 1, 1 } });
-        System.out.println("res2: ");
-        for (int n : res2) {
+        // int res1[] = sumAndMultiply("10203004", new int[][] { { 0, 7 }, { 1, 3 }, { 4, 6 } });
+        // System.out.println("res1: ");
+        // for (int n : res1) {
+        //     System.out.println(n);
+        // }
+        // int res2[] = sumAndMultiply("1000", new int[][] { { 0, 3 }, { 1, 1 } });
+        // System.out.println("res2: ");
+        // for (int n : res2) {
+        //     System.out.println(n);
+        // }
+        int res3[] = sumAndMultiply("9876543210", new int[][] { { 0, 9 } }); //output [444444137]
+        System.out.println("res3: ");
+        for (int n : res3) {
             System.out.println(n);
         }
     }
 
     public static int[] sumAndMultiply(String s, int[][] queries) {
-
-        int ans[] = new int[queries.length];
+        final int MOD = 1_000_000_007;
+        int[] ans = new int[queries.length];
 
         for (int i = 0; i < queries.length; i++) {
-            int x = 0;
-            int sum = 0;
+            long x = 0;
+            long sum = 0;
+
             for (int j = queries[i][0]; j <= queries[i][1]; j++) {
                 if (s.charAt(j) != '0') {
-                    x = (int) s.charAt(j) + x * 10;
-                    sum = (int) s.charAt(j) + sum;
+                    int digit = s.charAt(j) - '0';
+                    x = (x * 10 + digit) % MOD;
+                    sum = (sum + digit) % MOD;
                 }
             }
-            ans[i] = x * sum;
+
+            ans[i] = (int) ((x * sum) % MOD);
         }
+
         return ans;
     }
 }
